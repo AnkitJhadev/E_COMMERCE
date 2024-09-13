6c6734d9088a5adccd9c28596cda632783b58058
AnkitJhadev <jha80250@gmail.com>
2024-09-13 12:41:28 +0530

    created user model

---
+++ BACKEND/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = async (req, res) => {
+    res.send('Hello from Login');
+};
+
+export const signup = async (req, res) => {
+    res.send('Hello from signup');
+};
+
+export const logout = async (req, res) => {
+    res.send('Hello from logout');
+};
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,12 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import connectDb from "./lib/db.js"
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT =   3000;
+connectDb();  // Call to connect to MongoDB
 
+app.use("/api/auth", authRoutes);
 
-
-app.listen(PORT,()=>{
-      console.log("server is running on this "+PORT);
-})
+app.listen(PORT, () => {
+    console.log("server is running on this " + PORT);
+});
