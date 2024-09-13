cde6355c3e8805cd5f84cb4090ddebe41f681bab
AnkitJhadev <jha80250@gmail.com>
2024-09-13 18:40:19 +0530

    implement token based authentication for signup and login

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import connectDb from "./lib/db.js"
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
+
 const PORT =   3000;
 connectDb();  // Call to connect to MongoDB
 
