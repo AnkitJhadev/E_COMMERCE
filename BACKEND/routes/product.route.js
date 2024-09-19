a0250152bd2c01832cf56134c971e4474f92b310
AnkitJhadev <jha80250@gmail.com>
2024-09-19 10:50:42 +0530

    implemented featuredProduct functionality which all can acess

--- BACKEND/routes/product.route.js
+++ BACKEND/routes/product.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
 
 	getAllProducts,
+	getFeaturedProducts
 
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
@@ -9,6 +10,8 @@ import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+
 
 
 export default router;
