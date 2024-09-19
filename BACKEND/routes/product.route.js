d2507ceac53f27505bde1f7749acd3e56c535294
AnkitJhadev <jha80250@gmail.com>
2024-09-19 14:37:52 +0530

    Integrated clodinary to upload images

--- BACKEND/routes/product.route.js
+++ BACKEND/routes/product.route.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
 
 	getAllProducts,
-	getFeaturedProducts
+	getFeaturedProducts,
+	createProduct
 
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
@@ -11,6 +12,8 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+
 
 
 
