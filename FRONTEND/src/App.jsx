75c5b3eb63ec5d3f49c20679baf1addceffaedde
AnkitJhadev <jha80250@gmail.com>
2024-09-27 21:03:21 +0530

    Created a Fully Functional Cart Page Except payment integration

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -10,13 +10,24 @@ import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import { useCartStore } from "./stores/useCartStore";
 
 
 function App() {
 	const { user, checkAuth, checkingAuth } = useUserStore();
+	const { getCartItems } = useCartStore();
+
 	useEffect(() => {
 		checkAuth();
 	}, [checkAuth]);
+
+	useEffect(() => {
+		if (!user) return;
+
+		getCartItems();
+	}, [getCartItems, user]);
+
 	if (checkingAuth) return <LoadingSpinner />;
 
 
@@ -40,6 +51,8 @@ function App() {
 						element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}
 					/>
 					<Route path='/category/:category' element={<CategoryPage />} />
+					<Route path='/cart' element={user ? <CartPage /> : <Navigate to='/login' />} />
+
 
 				</Routes>
 			</div>
